e2bada2988fd3ebeff65f6cfe357a0845af02c2d
PriscilaSevero <priscila.severo@escolar.ifrn.edu.br>
2025-08-12 16:31:45 -0300

    12/08 - CA

--- My project (1)/Assets/Scripts/ControleAudio.cs
+++ My project (1)/Assets/Scripts/ControleAudio.cs
@@ -1,25 +1,52 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.Rendering;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class ControleAudio : MonoBehaviour
 {
     public int Volume;
     public int VolumeSFX;
+    public int VolumeFalas;
+    public int VolumeToque;
     public bool musica;
+    
     
     public Slider VolumeSlider;
     public Slider VolumeSFXSlider;
+    public Slider VolumeFalaSlider;
+    public Slider VolumeToqueSlider;
     public Toggle toggleMusica;
+    public TMP_Text textoMusica;
+    
     void Start()
     {
-        musica = toggleMusica.is
+        musica = toggleMusica.isOn;
+        Volume = (int)VolumeSlider.value;
+        VolumeSFX = (int)VolumeSFXSlider.value;
+        VolumeFalas = (int)VolumeFalaSlider.value;
+        VolumeToque = (int)VolumeToqueSlider.value;
     }
 
     
     void Update()
     {
-        
+        musica = toggleMusica.isOn;
+        Volume = (int)VolumeSlider.value;
+        VolumeSFX = (int)VolumeSFXSlider.value;
+        VolumeFalas = (int)VolumeFalaSlider.value;
+        VolumeToque = (int)VolumeToqueSlider.value;
+
+        if (musica ==  true)
+        {
+            textoMusica.text = "Ligado";
+            textoMusica.color = Color.green;
+        }
+        else
+        {
+            textoMusica.text = "Desligado";
+            textoMusica.color = Color.red; 
+        }
     }
 }
